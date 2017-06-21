34183050a8634f2158e1eed87bd8e6987879cea0
Hoyoung Jung <honux77@gmail.com>
2017-06-21 11:52:38 +0900

    Add sum of two numbers

---
+++ src/honux/calandar/Sum.java
@@ -0,0 +1,22 @@
+package honux.calandar;
+
+import java.util.Scanner;
+
+public class Sum {
+	public static void main(String[] args) {
+		int a,b;
+		
+		//입력: 키보드로 두 수의 입력을 받는다.
+		Scanner scanner = new Scanner(System.in);
+		String s1, s2;
+		System.out.println("두 수를 입력해 주세요");
+		s1 = scanner.next();
+		s2 = scanner.next();
+		a =  Integer.parseInt(s1);
+		b = Integer.parseInt(s2);
+		
+		//출력: 화면에 두 수의 합을 출력한다.
+		System.out.printf("%d와 %d의 합은 %d입니다",a, b, a + b);
+		scanner.close();
+	}
+}
