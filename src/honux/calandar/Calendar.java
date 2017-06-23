e67a333923f89ef3a7135038a44a6f850521775c
Hoyoung Jung <honux77@gmail.com>
2017-06-23 13:27:24 +0900

    Implement printing calendar (auto version)

--- src/honux/calandar/Calendar.java
+++ src/honux/calandar/Calendar.java
@@ -1,11 +1,9 @@
 package honux.calandar;
 
-import java.util.Scanner;
-
 public class Calendar {
 
-	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] MAX_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public boolean isLeapYear(int year) {
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
@@ -16,39 +14,94 @@ public class Calendar {
 
 	public int getMaxDaysOfMonth(int year, int month) {
 		if (isLeapYear(year)) {
-			return LEAP_MAX_DAYS[month - 1];
+			return LEAP_MAX_DAYS[month];
 		} else  {
-			return MAX_DAYS[month - 1];
+			return MAX_DAYS[month];
 		}
 	}
 
-	/**
-	 * - 월을 입력하면 해당월의 달력을 출력한다.
-	 * - 달력은 모양은 1단계에서 작성한 모양으로 만든다. 
-	 * - 1일은 일요일로 정해도 무방하다. 
-	 * - -1을 입력받기 전까지 반복 입력받는다. 
-	 * - 프롬프트를 출력한다.
-	 */
-
 	public void printCalendar(int year, int month) {
-		System.out.printf("    <<%4d년%3d월>>\n",year, month);
+		System.out.printf("    <<%d년 %d월>>\n",year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
 		
-		int maxDay = getMaxDaysOfMonth(year, month);
+		//get weekday automatically
+		int weekday = getWeekDay(year, month, 1); 
 		
-		for(int i = 1; i <= maxDay; i++) {
+		//print blank space 
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
+		
+		int maxDay = getMaxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int delim = (count < 7) ? count : 0;
+		/*
+		int delim;
+		if (count < 7) {
+			delim = count;
+		} else {
+			delim = 0;
+		}*/
+		
+		//print first line
+		for(int i = 1; i <= count; i++) {
 			System.out.printf("%3d",i);
-			if (i % 7 == 0) 
+		}
+		System.out.println();
+
+		//print from second line to last
+		
+		count++;
+		for(int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d",i);
+			if (i % 7 == delim) 
 				System.out.println();
 		}
 		
 		System.out.println();
-//		System.out.println(" 1  2  3  4  5  6  7");
-//		System.out.println(" 8  9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
+		System.out.println();
 	}
 
+	private int getWeekDay(int year, int month, int day) {
+		int syear = 1970;
+		final int STANDARD_WEEKDAY = 3; //1970/Jan/1st = Thursday
+		
+		int count = 0;
+		
+		for (int i = syear; i < year; i++) {
+			int delta = isLeapYear(i) ? 366 : 365;
+			count += delta;
+		}
+		
+		//System.out.println(count);
+		for (int i = 1; i < month; i++) {
+			int delta = getMaxDaysOfMonth(year, i);
+			count += delta;
+		}
+		
+		count += day;
+		
+		int weekday = (count + STANDARD_WEEKDAY) % 7;
+		return weekday;
+	}
 	
+	//simple test code here
+	public static void main(String[] args) {
+		Calendar cal = new Calendar();
+		System.out.println(cal.getWeekDay(1970, 1, 1) == 3);
+		System.out.println(cal.getWeekDay(1971, 1, 1) == 4);
+		System.out.println(cal.getWeekDay(1972, 1, 1) == 5);
+		System.out.println(cal.getWeekDay(1973, 1, 1) == 0);
+		System.out.println(cal.getWeekDay(1974, 1, 1) == 1);
+
+
+
+
+
+
+
+
+
+	}
 }
--- src/honux/calandar/Prompt.java
+++ src/honux/calandar/Prompt.java
@@ -3,29 +3,44 @@ package honux.calandar;
 import java.util.Scanner;
 
 public class Prompt {
-
-	private final static String PROMPT = "cal> ";
+	
+	/**
+	 * 
+	 * @param week 요일명
+	 * @return 0 ~ 6 (0 = Sunday, 6 = Saturday)
+	 */
+	public int parseDay(String week) {
+		if (week.equals("su")) return 0;
+		else if(week.equals("mo")) return 1;
+		else if(week.equals("tu")) return 2;
+		else if(week.equals("we")) return 3;
+		else if(week.equals("th")) return 4;
+		else if(week.equals("fr")) return 5;
+		else if(week.equals("sa")) return 6;
+		else
+			return 0;		
+	}
 
 	public void runPrompt() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		int month = -1;
-		int year = -1;
+		int month = 1;
+		int year = 2017;
 
 		while (true) {
-			System.out.println("년도를 입력하세요");
+			System.out.println("년도를 입력하세요.(exit: -1)");
 			System.out.print("YEAR> ");
 			year = scanner.nextInt();
-			System.out.println("달을 입력하세요");
+			if (year == -1)
+				break;
+			
+			System.out.println("달을 입력하세요.");
 			System.out.print("MONTH> ");
 			month = scanner.nextInt();
-			
-			if (month == -1) {
-				break;
-			}
 
-			if (month > 12) {
+			if (month > 12 || month < 1) {
+				System.out.println("잘못된 입력입니다.");
 				continue;
 			}
 
