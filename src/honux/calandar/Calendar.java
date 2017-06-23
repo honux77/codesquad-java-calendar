b3eccdf61f7311a43ff61524ea289c11b56784dc
Hoyoung Jung <honux77@gmail.com>
2017-06-23 15:13:40 +0900

    Implement register / search plan

--- src/honux/calandar/Calendar.java
+++ src/honux/calandar/Calendar.java
@@ -1,10 +1,41 @@
 package honux.calandar;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 public class Calendar {
 
 	private static final int[] MAX_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LEAP_MAX_DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
+	private HashMap <Date, String> planMap; 
+	
+	public Calendar() {
+		planMap = new HashMap<Date, String>();
+	}
+	
+	/**
+	 * 
+	 * @param date ex: "2017-06-20"
+	 * @param plan
+	 * @throws ParseException 
+	 */
+	public void registerPlan(String strDate, String plan) throws ParseException {
+
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+		//System.out.println(date);
+		planMap.put(date, plan);		
+	}
+	
+	public String searchPlan(String strDate) throws ParseException {
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+		String plan = planMap.get(date);
+		return plan;
+
+	}
+	
 	public boolean isLeapYear(int year) {
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
 			return true;
@@ -87,21 +118,16 @@ public class Calendar {
 	}
 	
 	//simple test code here
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Calendar cal = new Calendar();
-		System.out.println(cal.getWeekDay(1970, 1, 1) == 3);
-		System.out.println(cal.getWeekDay(1971, 1, 1) == 4);
-		System.out.println(cal.getWeekDay(1972, 1, 1) == 5);
-		System.out.println(cal.getWeekDay(1973, 1, 1) == 0);
-		System.out.println(cal.getWeekDay(1974, 1, 1) == 1);
-
-
-
-
-
-
-
-
+		System.out.println(cal.getWeekDay(1970, 1, 1) == 4);
+		System.out.println(cal.getWeekDay(1971, 1, 1) == 5);
+		System.out.println(cal.getWeekDay(1972, 1, 1) == 6);
+		System.out.println(cal.getWeekDay(1973, 1, 1) == 1);
+		System.out.println(cal.getWeekDay(1974, 1, 1) == 2);
+		
+		cal.registerPlan("2017-06-23", "Let's eat beef!");
+		System.out.println(cal.searchPlan("2017-06-23").equals("Let's eat beef!"));
 
 	}
 }
--- src/honux/calandar/Prompt.java
+++ src/honux/calandar/Prompt.java
@@ -1,57 +1,130 @@
 package honux.calandar;
 
+import java.text.ParseException;
 import java.util.Scanner;
 
 public class Prompt {
-	
+
+	public void printMenu() {
+		System.out.println("+----------------------+");
+		System.out.println("| 1. 일정 등록          ");
+		System.out.println("| 2. 일정 검색           ");
+		System.out.println("| 3. 달력 보기");
+		System.out.println("| h. 도움말 q. 종료");
+		System.out.println("+----------------------+");
+	}
+
 	/**
 	 * 
-	 * @param week 요일명
+	 * @param week
+	 *            요일명
 	 * @return 0 ~ 6 (0 = Sunday, 6 = Saturday)
 	 */
 	public int parseDay(String week) {
-		if (week.equals("su")) return 0;
-		else if(week.equals("mo")) return 1;
-		else if(week.equals("tu")) return 2;
-		else if(week.equals("we")) return 3;
-		else if(week.equals("th")) return 4;
-		else if(week.equals("fr")) return 5;
-		else if(week.equals("sa")) return 6;
+		if (week.equals("su"))
+			return 0;
+		else if (week.equals("mo"))
+			return 1;
+		else if (week.equals("tu"))
+			return 2;
+		else if (week.equals("we"))
+			return 3;
+		else if (week.equals("th"))
+			return 4;
+		else if (week.equals("fr"))
+			return 5;
+		else if (week.equals("sa"))
+			return 6;
 		else
-			return 0;		
+			return 0;
 	}
 
-	public void runPrompt() {
+	// +----------------------+
+	// | 1. 일정 등록
+	// | 2. 일정 검색
+	// | 3. 달력 보기
+	// | h. 도움말 q. 종료
+	// +----------------------+
+	// 명령 (1, 2, 3, h, q)
+
+	public void runPrompt() throws ParseException {
+		printMenu();
+
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		int month = 1;
-		int year = 2017;
-
 		while (true) {
-			System.out.println("년도를 입력하세요.(exit: -1)");
-			System.out.print("YEAR> ");
-			year = scanner.nextInt();
-			if (year == -1)
+			System.out.println("명령 (1, 2, 3, h, q)");
+			String cmd = scanner.next();
+			if (cmd.equals("1"))
+				cmdRegister(scanner, cal);
+			else if (cmd.equals("2"))
+				cmdSearch(scanner, cal);
+			else if (cmd.equals("3"))
+				cmdCal(scanner, cal);
+			else if (cmd.equals("h"))
+				printMenu();
+			else if (cmd.equals("q"))
 				break;
-			
-			System.out.println("달을 입력하세요.");
-			System.out.print("MONTH> ");
-			month = scanner.nextInt();
-
-			if (month > 12 || month < 1) {
-				System.out.println("잘못된 입력입니다.");
-				continue;
-			}
-
-			cal.printCalendar(year, month);
 		}
 
-		System.out.println("Bye~");
+		System.out.println("Thank you. Bye~");
 		scanner.close();
 	}
 
-	public static void main(String[] args) {
+	private void cmdCal(Scanner s, Calendar c) {
+
+		int month = 1;
+		int year = 2017;
+		System.out.println("년도를 입력하세요.");
+		System.out.print("YEAR> ");
+		year = s.nextInt();
+
+		System.out.println("달을 입력하세요.");
+		System.out.print("MONTH> ");
+		month = s.nextInt();
+
+		if (month > 12 || month < 1) {
+			System.out.println("잘못된 입력입니다.");
+			return;
+		}
+
+		c.printCalendar(year, month);
+	}
+
+	private void cmdSearch(Scanner s, Calendar c) {
+		System.out.println("[일정 검색]");
+		System.out.println("날짜를 입력해 주세요 (yyyy-MM-dd).");
+		String date = s.next();
+		String plan="";
+		try {
+			plan = c.searchPlan(date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			System.err.println("일정 검색 중 오류가 발생했습니다.");
+		}
+		System.out.println(plan);
+		
+	}
+
+	private void cmdRegister(Scanner s, Calendar c) throws ParseException {
+		System.out.println("[새 일정 등록]");
+		System.out.println("날짜를 입력해 주세요 (yyyy-MM-dd).");
+		String date = s.next();
+		String text = "";
+		System.out.println("일정을 입력해 주세요. (문장의 끝에 ;을 입력해 주세요.)");
+		while (true) {
+			String word = s.next();
+			text += word + " ";
+			if (word.endsWith(";")) {
+				break;
+			}
+		}
+		
+		c.registerPlan(date, text);
+	}
+
+	public static void main(String[] args) throws ParseException {
 		// 셸 실행
 		Prompt p = new Prompt();
 		p.runPrompt();
